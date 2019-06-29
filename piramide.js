c9fa1112acfa6e8c6237cef2e00bbd92ddd1cd16
jgersain <jhersein@gamil.com>
2019-06-29 13:18:34 -0500

    add other function

--- piramide.js
+++ piramide.js
@@ -1,19 +1,27 @@
 
 // for implementation
 function buildPyramid(size){
-    for (let symbol = 0; symbol < size; symbol++) {
+    for (let symbol = 1; symbol < size; symbol++) {
         console.log("0".repeat(symbol));
     }
 }
 
 // while implementation
 function buildPyramid2(size){
-    let counter = 0;
+    let counter = 1;
     while (counter < size) {
         console.log("0".repeat(counter));
         counter++;
     }
 }
 
-buildPyramid(30);
-buildPyramid2(30);
+function buildPyramid3(size) {
+    let asterick = "*";
+    for (let symbol = 1; symbol < size; symbol++) {
+        console.log(asterick += "*");
+    }
+}
+
+// buildPyramid(30);
+// buildPyramid2(30);
+buildPyramid3(30);
